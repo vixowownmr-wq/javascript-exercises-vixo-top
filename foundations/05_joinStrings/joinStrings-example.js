1dc94d675ab39979900944b3bdc4f9e70e6434b8
vixowownmr-wq <vixowow.nmr@gmail.com>
2026-05-10 23:48:02 -0400

    Resoluciones de 1 al 5 ejercicios de javascript :) vamos full motivado

--- foundations/05_joinStrings/joinStrings-example.js
+++ foundations/05_joinStrings/joinStrings-example.js
@@ -1,10 +1,14 @@
+const {fullName} = require("./joinStrings");
+const {age} = require("./joinStrings");
+
 const birthYear = 1947;
 const thisYear = 1965;
 const firstName = "Carlos";
 const lastName = "Stevenson";
 
-const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
+const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old.";
 
+//Hello! My name is Carlos Stevenson and I am n° years old.
 console.log(greeting);
 
 module.exports = {
--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -7,7 +7,22 @@
  The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
 */
 
+
+
 // Add your code right below, good luck!
+
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const thisYear = 1965;
+const birthYear = 1947;
+const fullName = firstName + " " + lastName;
+const age = thisYear - birthYear;
+
+const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old."
+
+
+
+console.log(greeting);
 
 
 
