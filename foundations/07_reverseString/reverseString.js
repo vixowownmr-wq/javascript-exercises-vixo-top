39689e930d7cffa45cc09116d762e4d01fbe6262
vixowownmr-wq <vixowow.nmr@gmail.com>
2026-05-27 16:52:48 -0400

    Ejercicio 7 reverseString :)

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let separarCadena = str.split("");
+    let invertirArreglo = separarCadena.reverse();
+    let unirCadena = invertirArreglo.join("");
+
+    return unirCadena;
+
+
 
 };
 
