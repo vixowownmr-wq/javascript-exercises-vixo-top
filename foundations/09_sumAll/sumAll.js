45a4255028e7833b017ccc41c0e9094526bacf6e
vixowownmr-wq <vixowow.nmr@gmail.com>
2026-06-03 13:29:24 -0400

    Ejercicio 9 sumAll Completado

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    const menor = Math.min(num1, num2);
+    const mayor = Math.max(num1, num2);
+
+    if(!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    }
+
+    if(num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+
+    let resultado = 0;
+
+    for(let i = menor; i <= mayor; i++) {
+        resultado += i;
+    }
+
+    return resultado;
 
 };
 
