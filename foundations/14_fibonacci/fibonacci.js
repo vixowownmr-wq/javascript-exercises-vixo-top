fd898c5180ded797b56164a6e196c5e003cb17b2
vixowownmr-wq <vixowow.nmr@gmail.com>
2026-07-17 19:19:24 -0400

    Ejercicio 14 Fibonaccio ready

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let previous = 1;
+    let current = 1;
+
+    n = Number(n);
+
+    if(n < 0) {
+        return "OOPS";
+    }
+
+    if(n === 0) {
+        return 0;
+    }
+
+    for(let i = 2; i < n; i++) {
+        let next = previous + current;
+
+        previous = current;
+        current = next;
+        
+    }
+    return current;
 
 };
 
